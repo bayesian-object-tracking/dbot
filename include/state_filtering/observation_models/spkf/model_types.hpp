fc33137175f94a190e14fb378907bf85db1bbf12
Issac <jan.issac@gmail.com>
2014-05-30 18:18:11 +0200

    Added spkf measurement model; NOTE: this will be redesigned!

---
+++ include/state_filtering/observation_models/spkf/model_types.hpp
@@ -0,0 +1,15 @@
+#ifndef MODEL_TYPES_HPP_
+#define MODEL_TYPES_HPP_
+
+namespace distr
+{
+    enum ModelType
+    {
+        Linear,
+        NonLinearWithAdditiveNoise,
+        NonLinearNonAdditiveNoise
+    };
+}
+
+
+#endif
