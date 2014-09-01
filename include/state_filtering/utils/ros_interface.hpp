eb914aa0a20c5d17032171fe4e9dece542d9f495
issac <jan.issac@gmail.com>
2014-09-01 17:24:44 +0200

    Moved implementation to cpp file and added explicit instantiation of the pcl interface template functions

--- include/state_filtering/utils/ros_interface.hpp
+++ include/state_filtering/utils/ros_interface.hpp
@@ -28,19 +28,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #ifndef POSE_FILTERING_ROS_INTERFACE_HPP_
 #define POSE_FILTERING_ROS_INTERFACE_HPP_
 
-
 #include <string>
+#include <limits>
+
 #include <Eigen/Core>
 #include <Eigen/Geometry>
-#include <limits>
 
 #include <ros/ros.h>
 #include <std_msgs/Header.h>
+#include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <visualization_msgs/Marker.h>
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl/ros/conversions.h>
+#include <sensor_msgs/CameraInfo.h>
 
+#include <pcl/ros/conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
@@ -70,50 +71,19 @@ void ReadParameter(const std::string& path,
 }
 
 template<>
-void ReadParameter< std::vector<std::string> >(const std::string& path,
-                                               std::vector<std::string>& parameter,
-                                               ros::NodeHandle node_handle)
-{
-    XmlRpc::XmlRpcValue ros_parameter;
-    node_handle.getParam(path, ros_parameter);
-    parameter.resize(ros_parameter.size());
-    for(size_t i = 0; i < parameter.size(); i++)
-        parameter[i] = std::string(ros_parameter[i]);
-}
-
+void ReadParameter<std::vector<std::string> >(const std::string& path,
+                                              std::vector<std::string>& parameter,
+                                              ros::NodeHandle node_handle);
 
 template<>
-void ReadParameter< std::vector<double> >(const std::string& path,
-                                               std::vector<double>& parameter,
-                                               ros::NodeHandle node_handle)
-{
-    XmlRpc::XmlRpcValue ros_parameter;
-    node_handle.getParam(path, ros_parameter);
-    parameter.resize(ros_parameter.size());
-    for(size_t i = 0; i < parameter.size(); i++)
-        parameter[i] = double(ros_parameter[i]);
-}
+void ReadParameter<std::vector<double> >(const std::string& path,
+                                         std::vector<double>& parameter,
+                                         ros::NodeHandle node_handle);
 
 template<>
-void ReadParameter< std::vector<std::vector<size_t> > >(const std::string& path,
-                                                        std::vector<std::vector<size_t> >& parameter,
-                                                        ros::NodeHandle node_handle)
-{
-    XmlRpc::XmlRpcValue ros_parameter;
-    node_handle.getParam(path, ros_parameter);
-    parameter.resize(ros_parameter.size());
-    for(size_t i = 0; i < parameter.size(); i++)
-    {
-        parameter[i].resize(ros_parameter[i].size());
-        for(size_t j = 0; j < parameter[i].size(); j++)
-            parameter[i][j] = int(ros_parameter[i][j]);
-    }
-}
-
-
-
-
-
+void ReadParameter<std::vector<std::vector<size_t> > >(const std::string& path,
+                                                       std::vector<std::vector<size_t> >& parameter,
+                                                       ros::NodeHandle node_handle);
 
 template<typename Scalar> Eigen::Matrix<Scalar, -1, -1>
 Ros2Eigen(const sensor_msgs::Image& ros_image,
@@ -130,10 +100,6 @@ Ros2Eigen(const sensor_msgs::Image& ros_image,
 
     return eigen_image;
 }
-
-
-
-
 
 template<typename Scalar> Eigen::Matrix<Scalar, 3, 3>
 GetCameraMatrix(const std::string& camera_info_topic,
@@ -167,101 +133,22 @@ void PublishMarker(const Eigen::Matrix3f R, const Eigen::Vector3f t,
 		std_msgs::Header header,
 		std::string object_model_path,
 		const ros::Publisher &pub,
-		int marker_id = 0, float r = 0, float g = 1, float b = 0, float a = 1.0)
-{
+        int marker_id = 0, float r = 0, float g = 1, float b = 0, float a = 1.0);
 
-	Eigen::Quaternion<float> q(R);
-
-	geometry_msgs::PoseWithCovarianceStamped pose;
-	pose.header =  header;
-	pose.pose.pose.position.x = t(0);
-	pose.pose.pose.position.y = t(1);
-	pose.pose.pose.position.z = t(2);
-
-	pose.pose.pose.orientation.x = q.x();
-	pose.pose.pose.orientation.y = q.y();
-	pose.pose.pose.orientation.z = q.z();
-	pose.pose.pose.orientation.w = q.w();
-
-
-	visualization_msgs::Marker marker;
-	marker.header.frame_id =  pose.header.frame_id;
-	marker.header.stamp =  pose.header.stamp;
-	marker.ns = "object_pose_estimation";
-	marker.id = marker_id;
-	marker.scale.x = 1.0;
-	marker.scale.y = 1.0;
-	marker.scale.z = 1.0;
-	marker.color.r = r;
-	marker.color.g = g;
-	marker.color.b = b;
-	marker.color.a = a;
-
-	marker.type = visualization_msgs::Marker::MESH_RESOURCE;
-	marker.action = visualization_msgs::Marker::ADD;
-	marker.pose = pose.pose.pose;
-
-	marker.mesh_resource = object_model_path;
-
-	pub.publish(marker);
-}
 void PublishMarker(const Eigen::Matrix4f H,
 		std_msgs::Header header,
 		std::string object_model_path,
 		const ros::Publisher &pub,
-		int marker_id = 0, float r = 0, float g = 1, float b = 0, float a = 1.0)
-{
-    PublishMarker(H.topLeftCorner(3,3),
-                  H.topRightCorner(3,1),
-                  header,
-                  object_model_path, pub,
-                  marker_id, r, g, b, a);
-
-}
-
-
+        int marker_id = 0, float r = 0, float g = 1, float b = 0, float a = 1.0);
 
 
 
 void PublishPoints(const std_msgs::Header header,
 		const ros::Publisher &pub,
-		const std::vector<Eigen::Vector3f> points, std::vector<float> colors = std::vector<float>(0),
-		const Eigen::Matrix3f R = Eigen::Matrix3f::Identity(), Eigen::Vector3f t = Eigen::Vector3f::Zero())
-{
-	// if no color has been given we set it to some value -----------------------------
-	if(colors.size() == 0)
-		colors = std::vector<float> (points.size(), 1);
-
-	// renormalize colors -----------------------------
-	float max = -std::numeric_limits<float>::max();
-	float min = std::numeric_limits<float>::max();
-	for(int i = 0; i < int(colors.size()); i++)
-	{
-		min = colors[i] < min ? colors[i] : min;
-		max = colors[i] > max ? colors[i] : max;
-	}
-	if(min == max) min = 0;
-
-	pcl::PointCloud<pcl::PointXYZRGB> point_cloud;
-	point_cloud.header = header;
-	point_cloud.width    = points.size(); point_cloud.height   = 1; point_cloud.is_dense = false;
-	point_cloud.points.resize (point_cloud.width * point_cloud.height);
-
-	for (int point_index = 0; point_index < int(points.size()); ++point_index)
-	{
-		Eigen::Vector3f new_point = R*points[point_index] + t;
-		point_cloud.points[point_index].x = new_point(0);
-		point_cloud.points[point_index].y = new_point(1);
-		point_cloud.points[point_index].z = new_point(2);
-
-		point_cloud.points[point_index].r = (colors[point_index]-min)/(max-min) * 255.;
-		point_cloud.points[point_index].g = 0.;
-		point_cloud.points[point_index].b = (1 - (colors[point_index]-min)/(max-min)) * 255.;
-	}
-	sensor_msgs::PointCloud2 point_cloud2;
-	pcl::toROSMsg(point_cloud, point_cloud2);
-	pub.publish(point_cloud2);
-}
+        const std::vector<Eigen::Vector3f> points,
+        std::vector<float> colors = std::vector<float>(0),
+        const Eigen::Matrix3f R = Eigen::Matrix3f::Identity(),
+        Eigen::Vector3f t = Eigen::Vector3f::Zero());
 
 }
 
