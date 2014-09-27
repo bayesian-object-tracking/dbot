f2f2db69cfa81c9958daebe4a080d70a08d19d46
issac <jan.issac@gmail.com>
2014-09-27 19:10:54 +0200

    Returns Eigen::Matrices instead of scalars

--- include/pose_tracking/models/process_models/continuous_occlusion_process_model.hpp
+++ include/pose_tracking/models/process_models/continuous_occlusion_process_model.hpp
@@ -39,25 +39,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 namespace ff
 {
 
+// Forward declarations
+class ContinuousOcclusionProcessModel;
+
+namespace internal
+{
+template <>
+struct Traits<ContinuousOcclusionProcessModel>
+{
+    typedef double                      Scalar;
+    typedef Eigen::Matrix<Scalar, 1, 1> State;
+    typedef Eigen::Matrix<Scalar, 1, 1> Noise;
+
+    typedef StationaryProcessModel<State> ProcessModelBase;
+    typedef GaussianMap<State, Noise>     GaussianMapBase;
+};
+}
+
 class ContinuousOcclusionProcessModel:
-        public StationaryProcessModel<double>,
-        public GaussianMap<double, double>
+        public internal::Traits<ContinuousOcclusionProcessModel>::ProcessModelBase,
+        public internal::Traits<ContinuousOcclusionProcessModel>::GaussianMapBase
 {
 public:
+    typedef internal::Traits<ContinuousOcclusionProcessModel> Traits;
+    typedef typename Traits::Scalar Scalar;
+    typedef typename Traits::State  State;
+    typedef typename Traits::Noise  Noise;
+
 	// the prob of source being object given source was object one sec ago,
 	// and prob of source being object given one sec ago source was not object
-    ContinuousOcclusionProcessModel(double p_occluded_visible,
-                                    double p_occluded_occluded,
-                                    double sigma):
+    ContinuousOcclusionProcessModel(Scalar p_occluded_visible,
+                                    Scalar p_occluded_occluded,
+                                    Scalar sigma):
         mean_(p_occluded_visible, p_occluded_occluded),
         sigma_(sigma) { }
 
     virtual ~ContinuousOcclusionProcessModel() {}
 
     virtual void Condition(const double& delta_time,
-                           const double& occlusion)
+                           const State& occlusion)
     {
-        double initial_occlusion_probability = hf::Sigmoid(occlusion);
+        double initial_occlusion_probability = hf::Sigmoid(occlusion(0, 0));
 
         mean_.Condition(delta_time, initial_occlusion_probability);
         double mean = mean_.MapStandardGaussian();
@@ -66,9 +88,17 @@ public:
               TruncatedGaussian(mean, sigma_ * std::sqrt(delta_time), 0.0, 1.0);
     }
 
-    virtual double MapStandardGaussian(const double& sample) const
+    virtual State MapStandardGaussian(const Noise& sample) const
     {
-        return hf::Logit(occlusion_probability_.MapStandardGaussian(sample));
+        State l;
+        l(0, 0) = hf::Logit(
+                    occlusion_probability_.MapStandardGaussian(sample(0,0)));
+        return l;
+    }
+
+    virtual size_t Dimension()
+    {
+        return 1;
     }
 
 private:
