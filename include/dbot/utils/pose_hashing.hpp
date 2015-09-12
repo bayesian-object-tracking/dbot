986c79cf412805fe031056f45c5e2feb65cf711d
issac <jan.issac@gmail.com>
2015-09-12 18:28:36 +0200

    Added hashing for PoseVelocityVector

--- include/dbot/utils/pose_hashing.hpp
+++ include/dbot/utils/pose_hashing.hpp
@@ -26,13 +26,42 @@
 
 #include <fl/util/types.hpp>
 #include <fl/util/math/pose_vector.hpp>
+#include <fl/util/math/pose_velocity_vector.hpp>
 
 template <typename Vector> class PoseHash;
 
 template <> class PoseHash<fl::PoseVector>
 {
 public:
-    std::size_t operator()(const fl::PoseVector & s) const
+    std::size_t operator()(const fl::PoseVector& s) const
+    {
+        /* primes */
+        static constexpr int p1 = 15487457;
+        static constexpr int p2 = 24092821;
+        static constexpr int p3 = 73856093;
+        static constexpr int p4 = 19349663;
+        static constexpr int p5 = 83492791;
+        static constexpr int p6 = 17353159;
+
+        /* map size */
+        static constexpr int n = 1200;
+
+        /* precision */
+        static constexpr int c = 1000000;
+
+        return  ((int(s(0, 0) * c) * p1) ^
+                 (int(s(1, 0) * c) * p2) ^
+                 (int(s(2, 0) * c) * p3) ^
+                 (int(s(3, 0) * c) * p4) ^
+                 (int(s(4, 0) * c) * p5) ^
+                 (int(s(5, 0) * c) * p6) % n);
+    }
+};
+
+template <> class PoseHash<fl::PoseVelocityVector>
+{
+public:
+    std::size_t operator()(const fl::PoseVelocityVector& s) const
     {
         /* primes */
         static constexpr int p1 = 15487457;
