99e3f37646fa8ca1a2565ce415b984680cc73418
issac <jan.issac@gmail.com>
2015-10-10 15:35:57 +0200

    Removed doc configuration from version.cmake

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,12 +1,6 @@
-
 find_package(Git)
 
 execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --always
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 OUTPUT_VARIABLE PROJECT_VERSION
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-# update version in documentation config
-#configure_file(
-#    ${PROJECT_SOURCE_DIR}/doc/Doxyfile.in
-#    ${PROJECT_BINARY_DIR}/Doxyfile @ONLY)
